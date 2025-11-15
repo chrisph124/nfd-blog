bf68b893bbe3872f7ef70a2c1578275fa09b094e
Hieu (Chris) Pham <chris.pham124@gmail.com>
2025-11-16 00:13:08 +0700

    Add Storyblok CMS integration with type generation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'a.storyblok.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'a2.storyblok.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
