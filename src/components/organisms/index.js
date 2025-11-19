d03778a6483083f1c0d2ab6651fcbf26389a0332
Hieu (Chris) Pham <chris.pham124@gmail.com>
2025-11-19 23:51:45 +0700

    Add Hero organism component

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -2,3 +2,4 @@ export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as Grid } from './Grid';
 export { default as NavBar } from './NavBar';
+export { default as Hero } from './Hero';
